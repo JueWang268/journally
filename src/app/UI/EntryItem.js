edcff72c1978f10f6a69920ec095138c8dc39d82
JueWang268 <yywangjue@gmail.com>
2024-10-24 14:28:18 -0400

    remove datepicker import

--- src/app/UI/EntryItem.js
+++ src/app/UI/EntryItem.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import './JournalSideBar.css'
 import dateFormat from '../../config/dateFormat'
-import "react-datepicker/dist/react-datepicker.css"
+// import "react-datepicker/dist/react-datepicker.css"
 
 export default function EntryItem(
     {entry, 
