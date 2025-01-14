99c4cafc78ffed982af5b5751a02457be35dbb1f
David Lee <72262475+dql1116@users.noreply.github.com>
2025-01-13 18:00:13 -0800

    Settings - Account, Notifications
    Worked on Account and Notifications

---
+++ src/app/settings/page.js
@@ -0,0 +1,186 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import '../../styles/Settings.css';
+import TopBar from '../UI/TopBar.jsx';
+import Image from 'next/image';
+
+const TABS = ["Account", "Notifications", "Privacy", "Appearance", "Preferences", "Subscription"];
+export default function SettingsPage() {
+    
+    const [selectedTab, setSelectedTab] = useState();
+    const [accountInfo, setAccountInfo] = useState({
+        profileImg: "/assets/UserProfile.png", 
+        name: "John Doe",
+        birth: "November 15, 2024",
+        gender: "Male",
+        email: "john.doe@gmail.com",
+        phone: "(123) 456-7890",
+    });
+
+    const [notifInfo, setNotifInfo] = useState({
+        email: false,
+        sms: false,
+        push: false,
+    });
+
+    const [editingField, setEditingField] = useState(null);
+
+    const toggleNotification = (field) => {
+        setNotifInfo((prevSettings) => ({
+            ...prevSettings,
+            [field]: !prevSettings[field],
+        }));
+    };
+    
+    const handleInputChange = (field, value) => {
+        setAccountInfo((prevInfo) => ({
+            ...prevInfo,
+            [field]: value,
+        }));
+    };
+
+    const handleKeyDown = (e, field) => {
+        if (e.key === "Enter"){
+            setEditingField(null);
+        }
+    };
+
+    // const renderAccountFields = () =>
+    //     Object.keys(accountInfo).map((field) => (
+    //         <div className='form-group' key={field}>
+    //             <label>{field.charAt(0).toUpperCase() + field.slice(1)}</label>
+    //             {editingField === field ? (
+    //                 <input
+    //                     type="text"
+    //                     value={accountInfo[field]}
+    //                     onChange={(e) => handleInputChange(field, e.target.value)}
+    //                     onBlur={() => setEditingField(null)}
+    //                     onKeyDown={(e) => handleKeyDown(e, field)}
+    //                 />
+    //             ) : (
+    //                 <span onClick={() => setEditingField(field)} className='editable-text'>
+    //                     {accountInfo[field]}
+    //                 </span>
+    //             )}
+    //         </div>
+    //     )) 
+    const renderAccountFields = () =>
+        Object.keys(accountInfo).map((field) => (
+            <div className="form-group" key={field}>
+                <label>{field === "profileImage" ? "Profile Image" : field.charAt(0).toUpperCase() + field.slice(1)}</label>
+                {field === "profileImg" ? (
+                    <div className="profile-image-container">
+                        <img
+                            src={accountInfo[field]}
+                            alt="Profile"
+                            className="profile-image"
+                        />
+                    </div>
+                ) : editingField === field ? (
+                    <input
+                        type="text"
+                        value={accountInfo[field]}
+                        onChange={(e) => handleInputChange(field, e.target.value)}
+                        onBlur={() => setEditingField(null)}
+                        onKeyDown={(e) => handleKeyDown(e, field)}
+                    />
+                ) : (
+                    <span
+                        onClick={() => setEditingField(field)}
+                        className="editable-text"
+                    >
+                        {accountInfo[field]}
+                    </span>
+                )}
+            </div>
+        ));
+    
+    const renderNotificationSettings = () =>
+        Object.keys(notifInfo).map((setting) => (
+            <div className="notifications-form-group" key={setting}>
+                <label>{setting.charAt(0).toUpperCase() + setting.slice(1).replace(/([A-Z])/g, ' $1')}</label>
+                <div className="view-switch">
+                    <div
+                        className={`slider ${notifInfo[setting] ? 'right' : 'left'}`}
+                        onClick={() => toggleNotification(setting)}
+                    >
+                        <span className="slider-text">{notifInfo[setting] ? 'On' : 'Off'}</span>
+                        <div className="slider-button"></div>
+                    </div>
+                </div>
+            </div>
+        ));
+
+    const renderContent = () => {
+        switch (selectedTab){
+            case "Account":
+                return (
+                    <div className='account-container'>
+                        <p>Account</p>
+                        <div className='account-content'>
+                            <p>Basic Info</p>
+                            <div className='content-list'>
+                                {renderAccountFields()}
+                            </div>
+                        </div>
+                    </div>
+                );
+            case "Notifications":
+                return (
+                    <div className='notifications-container'>
+                        <p>Notifications</p>
+                        <div className='notifications-content'>
+                            <p>Preferences</p>
+                            <div className='notifications-content-list'>
+                                {renderNotificationSettings()}
+                            </div>
+                        </div>
+                    </div>
+                );
+            case "Privacy":
+                return <div><h1>Privacy</h1></div>;
+            case "Appearance":
+                return <div><h1>Appearance</h1></div>;
+            case "Preferences":
+                return <div><h1>Preferences</h1></div>;
+            case "Subscription":
+                return <div><h1>Subscription</h1></div>;
+            default:
+                return <div>Select a tab to view its settings</div>;
+        }
+    };
+
+    return (
+      <div className='app'>
+        <TopBar />
+
+        <div className='home-container'>
+            <div className='content-box'>
+                <div className='content-grid'>
+                    <div className='side-bar'>
+                        <p>
+                            Settings
+                        </p>
+                        <ul>
+                            {TABS.map((tab) => (
+                                <li
+                                    key={tab}
+                                    className={`nav-pill ${selectedTab === tab ? "selected" : ""}`}
+                                    onClick={() => setSelectedTab(tab)}
+                                >
+                                    {tab}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className='content'>
+                        {renderContent()}
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+  
+  
